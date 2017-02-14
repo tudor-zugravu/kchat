48866088e35fef31a78d427c592bc233dc24078f
Tudor Vasile <tudor-dan@hotmail.co.uk>
2017-02-14 15:01:33 +0000

    Comiting interfaces and........................part2

--- AndroidApp/LoginRegisterDesign/app/src/androidTest/java/IMPL/DatabaseAdaptor.java
+++ AndroidApp/LoginRegisterDesign/app/src/androidTest/java/IMPL/DatabaseAdaptor.java
@@ -15,7 +15,7 @@ import API.IAdaptors;
 public class DatabaseAdaptor extends SQLiteOpenHelper implements IAdaptors  {
 
 
-    // Database Versionaa
+    // Database Version1
     private static final int DATABASE_VERSION = 1;
     // Database Name
     private static final String DATABASE_NAME = "ChatMobileDB";
