c168beb889668a93b813475a36274968ca2f76ed
Tudor Vasile <tudor-dan@hotmail.co.uk>
2017-02-14 15:54:24 +0000

    testing something part2

--- AndroidApp/LoginRegisterDesign/app/src/androidTest/java/IMPL/DatabaseAdaptor.java
+++ AndroidApp/LoginRegisterDesign/app/src/androidTest/java/IMPL/DatabaseAdaptor.java
@@ -15,7 +15,7 @@ import API.IAdaptors;
 public class DatabaseAdaptor extends SQLiteOpenHelper implements IAdaptors  {
 
 
-    // Database ----test2
+    // Database
     private static final int DATABASE_VERSION = 1;
     // Database Name
     private static final String DATABASE_NAME = "ChatMobileDB";
