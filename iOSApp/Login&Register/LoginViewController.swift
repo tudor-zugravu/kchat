410982eb72061aeaf9b143eef810d16bf7a0b1ae
Tudor Zugravu <tudorzg@Tudors-MacBook-Pro.local>
2017-02-15 14:09:39 +0000

    fix commits

--- iOSApp/Login&Register/LoginViewController.swift
+++ iOSApp/Login&Register/LoginViewController.swift
@@ -28,42 +28,74 @@ class LoginViewController: UIViewController {
         
         
         
-        if UserDefaults.standard.object(forKey: "userName") != nil && UserDefaults.standard.object(forKey: "userPwd") != nil {
-            let userNameDefault:String=UserDefaults.standard.object(forKey: "userName") as! String;
-            let userPwdDefault=UserDefaults.standard.object(forKey: "userPwd") as! String;
+//        if UserDefaults.standard.object(forKey: "userName") != nil && UserDefaults.standard.object(forKey: "userPwd") != nil {
+//            let userNameDefault:String=UserDefaults.standard.object(forKey: "userName") as! String;
+//            let userPwdDefault=UserDefaults.standard.object(forKey: "userPwd") as! String;
+//            
+//            if userNameDefault == userName{
+//                if userPwdDefault == userPwd{
+//                    //login is successful (the following is just use default to store it)
+//                    UserDefaults.standard.set(true, forKey: "isUserLogin");
+//                    UserDefaults.standard.synchronize()
+//                    //self.dismiss(animated: true, completion: nil)
+//                    
+//                    // Alert for success
+//                    let myAlert = UIAlertController(title:"Success!", message:"Logged in", preferredStyle:.alert);
+//                    let okaction=UIAlertAction(title:"ok", style:UIAlertActionStyle.default, handler:nil);
+//                    myAlert.addAction(okaction);
+//                    self.present(myAlert, animated:true, completion:nil);
+//                } else {
+//                    // Alert for wrong password
+//                    let myAlert = UIAlertController(title:"Error!", message:"Wrong password", preferredStyle:.alert);
+//                    let okaction=UIAlertAction(title:"ok", style:UIAlertActionStyle.default, handler:nil);
+//                    myAlert.addAction(okaction);
+//                    self.present(myAlert, animated:true, completion:nil);
+//                }
+//            } else {
+//                // Alert for wrong username
+//                let myAlert = UIAlertController(title:"Error!", message:"Wrong username", preferredStyle:.alert);
+//                let okaction=UIAlertAction(title:"ok", style:UIAlertActionStyle.default, handler:nil);
+//                myAlert.addAction(okaction);
+//                self.present(myAlert, animated:true, completion:nil);
+//            }
+//        } else {
+//            // Alert for no default values
+//            let myAlert = UIAlertController(title:"Error!", message:"No default values", preferredStyle:.alert);
+//            let okaction=UIAlertAction(title:"ok", style:UIAlertActionStyle.default, handler:nil);
+//            myAlert.addAction(okaction);
+//            self.present(myAlert, animated:true, completion:nil);
+//        }
+        
+        let url: URL = URL(string: "http://188.166.157.62:3000/users")!
+        let session = URLSession.shared
+        
+        var request = URLRequest(url:url)
+        request.httpMethod = "GET"
+        request.cachePolicy = NSURLRequest.CachePolicy.reloadIgnoringCacheData
+        
+//        let paramString = "email=\(email)&password=\(password)"
+//        request.httpBody = paramString.data(using: String.Encoding.utf8)
+        
+        let task = session.dataTask(with: request, completionHandler: {
+            (
+            data, response, error) in
             
-            if userNameDefault == userName{
-                if userPwdDefault == userPwd{
-                    //login is successful (the following is just use default to store it)
-                    UserDefaults.standard.set(true, forKey: "isUserLogin");
-                    UserDefaults.standard.synchronize()
-                    //self.dismiss(animated: true, completion: nil)
-                    
-                    // Alert for success
-                    let myAlert = UIAlertController(title:"Success!", message:"Logged in", preferredStyle:.alert);
-                    let okaction=UIAlertAction(title:"ok", style:UIAlertActionStyle.default, handler:nil);
-                    myAlert.addAction(okaction);
-                    self.present(myAlert, animated:true, completion:nil);
-                } else {
-                    // Alert for wrong password
-                    let myAlert = UIAlertController(title:"Error!", message:"Wrong password", preferredStyle:.alert);
-                    let okaction=UIAlertAction(title:"ok", style:UIAlertActionStyle.default, handler:nil);
-                    myAlert.addAction(okaction);
-                    self.present(myAlert, animated:true, completion:nil);
-                }
-            } else {
-                // Alert for wrong username
-                let myAlert = UIAlertController(title:"Error!", message:"Wrong username", preferredStyle:.alert);
-                let okaction=UIAlertAction(title:"ok", style:UIAlertActionStyle.default, handler:nil);
-                myAlert.addAction(okaction);
-                self.present(myAlert, animated:true, completion:nil);
+            guard let _:Data = data, let _:URLResponse = response, error == nil else {
+                print("error")
+                return
             }
-        } else {
-            // Alert for no default values
-            let myAlert = UIAlertController(title:"Error!", message:"No default values", preferredStyle:.alert);
-            let okaction=UIAlertAction(title:"ok", style:UIAlertActionStyle.default, handler:nil);
-            myAlert.addAction(okaction);
-            self.present(myAlert, animated:true, completion:nil);
-        }
+            
+            let dataString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+            
+            DispatchQueue.main.async(execute: { () -> Void in
+                
+                print(dataString!)
+            })
+            
+        })
+        
+        task.resume()
+        
+        
     }
 }
