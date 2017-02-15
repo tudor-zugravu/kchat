d43c413b81f4fa91abf59cd870d7a112fbcd123a
Tudor Vasile <tudor-dan@hotmail.co.uk>
2017-02-15 16:29:09 +0000

    re commit changes of today part 4

--- AndroidApp/LoginRegisterDesign/app/src/androidTest/java/IMPL/DatabaseAdaptor.java
+++ AndroidApp/LoginRegisterDesign/app/src/androidTest/java/IMPL/DatabaseAdaptor.java
@@ -1,12 +1,22 @@
 package IMPL;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 import API.IAdaptors;
+import API.IContacts;
+import API.IGroups;
+import API.IMessage;
 
 /**
  * Created by Tudor Vasile on 2/11/2017.
@@ -71,18 +81,182 @@ public class DatabaseAdaptor extends SQLiteOpenHelper implements IAdaptors  {
 
     @Override
     public boolean addToContactsTable(int contactId, int globalId, String contactName, String username) {
-        return false;
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put("id", contactId);
+        values.put("global_id", globalId);
+        values.put("name", contactName);
+        values.put("username", username);
+        db.insert("contacts", // table
+                null, //nullColumnHack
+                values); // key/value -> keys = column names/ values = column values
+        db.close();
+        return true;
     }
 
     @Override
-    public boolean addToGroupsTable(int groupId, String globalId, String name, String username) {
-        return false;
+    public boolean addToGroupsTable(int id, String name, String description, int type) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put("id", id);
+        values.put("name", name);
+        values.put("description", description);
+        values.put("type", type);
+        db.insert("groups", // table
+                null, //nullColumnHack
+                values); // key/value -> keys = column names/ values = column values
+        db.close();
+        return true;
     }
 
     @Override
     public boolean addToMessagesTable(int messageId, int senderId, int groupId, String message, Date timestamp) {
-        return false;
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put("id", messageId);
+        values.put("sender_id", senderId);
+        values.put("group_id", groupId);
+        values.put("message", message);
+        values.put("timestamp",timestamp.toString());
+        db.insert("messages", // table
+                null, //nullColumnHack
+                values); // key/value -> keys = column names/ values = column values
+        db.close();
+        return true;
     }
+
+    private static final String[] COLUMNS = {"id","global_id","name","username"};
+    public IContacts getContact(int contactId){
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor =
+                db.query("contacts", // a. table
+                        COLUMNS, // b. column names
+                        " id = ?", // c. selections
+                        new String[] { String.valueOf(contactId) }, // d. selections args
+                        null, // e. group by
+                        null, // f. having
+                        null, // g. order by
+                        null); // h. limit
+        // if we got results get the first one
+        if (cursor != null)
+            cursor.moveToFirst();
+        //  build the object
+        IContacts contact = new Contacts(Integer.parseInt(cursor.getString(0)),Integer.parseInt(cursor.getString(1)),cursor.getString(2),cursor.getString(3));
+        Log.d("getBook("+contactId+")", contact.toString());
+        return contact;
+    }
+
+    private static final String[] COLUMNS2 = {"id","sender_id","group_id","message","timestamp"};
+    public IMessage getMessage(int messageId){
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor =
+                db.query("messages", // a. table
+                        COLUMNS2, // b. column names
+                        " id = ?", // c. selections
+                        new String[] { String.valueOf(messageId) }, // d. selections args
+                        null, // e. group by
+                        null, // f. having
+                        null, // g. order by
+                        null); // h. limit
+        // if we got results get the first one
+        if (cursor != null)
+            cursor.moveToFirst();
+        //  build the object
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss aa");
+        Date convertedDate = new Date();
+        try {
+            convertedDate = dateFormat.parse(cursor.getString(4));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        IMessage message = new Message(Integer.parseInt(cursor.getString(0)),Integer.parseInt(cursor.getString(1)),Integer.parseInt(cursor.getString(2)),cursor.getString(3),convertedDate);
+        Log.d("getBook("+messageId+")", message.toString());
+        return message;
+    }
+
+    private static final String[] COLUMNS3 = {"id","name","description","type"};
+    public IGroups getGroup(int contactId){
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor =
+                db.query("groups", // a. table
+                        COLUMNS3, // b. column names
+                        " id = ?", // c. selections
+                        new String[] { String.valueOf(contactId) }, // d. selections args
+                        null, // e. group by
+                        null, // f. having
+                        null, // g. order by
+                        null); // h. limit
+        // if we got results get the first one
+        if (cursor != null)
+            cursor.moveToFirst();
+        //  build the object
+        IGroups group = new Groups(Integer.parseInt(cursor.getString(0)),cursor.getString(1),cursor.getString(2),Integer.parseInt(cursor.getString(3)));
+        Log.d("getBook("+contactId+")", group.toString());
+        return group;
+    }
+
+    public List<IContacts> getAllContacts() {
+        List<IContacts> contacts = new LinkedList<>();
+        String query = "SELECT  * FROM " + "contacts";
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+        IContacts contact = null;
+        if (cursor.moveToFirst()) {
+            do {
+                contact = new Contacts(Integer.parseInt(cursor.getString(0)),Integer.parseInt(cursor.getString(1)),cursor.getString(2),cursor.getString(3));
+                // Add book to books
+                contacts.add(contact);
+            } while (cursor.moveToNext());
+        }
+        Log.d("getAllBooks()", contacts.toString());
+        return contacts;
+    }
+
+    public List<IGroups> getAllGroups() {
+        List<IGroups> groups = new LinkedList<>();
+        String query = "SELECT  * FROM " + "groups";
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+        IGroups group = null;
+        if (cursor.moveToFirst()) {
+            do {
+                group = new Groups(Integer.parseInt(cursor.getString(0)),cursor.getString(1),cursor.getString(2),Integer.parseInt(cursor.getString(3)));
+                // Add book to books
+                groups.add(group);
+            } while (cursor.moveToNext());
+        }
+        Log.d("getAllBooks()", groups.toString());
+        return groups;
+    }
+
+    public List<IMessage> getAllMessages() {
+        List<IMessage> messages = new LinkedList<>();
+        String query = "SELECT  * FROM " + "messages";
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss aa");
+        Date convertedDate = new Date();
+        try {
+            convertedDate = dateFormat.parse(cursor.getString(4));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        IMessage message = null;
+        if (cursor.moveToFirst()) {
+            do {
+                 message = new Message(Integer.parseInt(cursor.getString(0)),Integer.parseInt(cursor.getString(1)),Integer.parseInt(cursor.getString(2)),cursor.getString(3),convertedDate);
+                // Add book to books
+                messages.add(message);
+            } while (cursor.moveToNext());
+        }
+        Log.d("getAllBooks()", messages.toString());
+        return messages;
+    }
+
 
     @Override
     public boolean flushData() {
