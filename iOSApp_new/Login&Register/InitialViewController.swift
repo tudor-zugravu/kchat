99db3e3211522bd7366882e9fb8634d619b6eb31
YiliaGong <k1623448@kcl.ac.uk>
2017-03-14 19:30:15 +0000

    add new ios-file in new version for back up

---
+++ iOSApp_new/Login&Register/InitialViewController.swift
@@ -0,0 +1,32 @@
+//
+//  InitialViewController.swift
+//  Login&Register
+//
+//  Created by 骧小爷 on 2017/2/6.
+//  Copyright © 2017年 骧小爷. All rights reserved.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        
+        // If a user is already logged in, proceed to the messages view
+        if UserDefaults.standard.bool(forKey: "hasLoginKey") == true {
+            self.performSegue(withIdentifier: "initialTabBarViewController", sender: nil)
+        } else {
+            self.performSegue(withIdentifier: "loginView", sender: self)
+        }
+    }
+}
+
