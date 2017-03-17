3f250d15d70534033df2eecaf1c54968c9643bf0
Tudor Vasile <tudor-dan@hotmail.co.uk>
2017-03-17 13:17:56 +0000

    Private chat ui almost finished

--- AndroidApp/LoginRegisterDesign/app/src/main/java/IMPL/Message.java
+++ AndroidApp/LoginRegisterDesign/app/src/main/java/IMPL/Message.java
@@ -19,45 +19,20 @@ public class Message implements IMessage {
     private int senderId;
     private int groupId;
     private String message;
-    private Date timestamp;
+    private String timestamp;
     private String username;
     private int imageId;
     private boolean isMe;
     private int receiverId;
     private String strTimestamp;
-    private Bitmap bitmap;
 
-    public Message(int messageId,int groupId,String message,Date timestamp,String username){
-    this.messageId = messageId;
-        this.groupId = groupId;
-        this.message = message;
-        this.timestamp = timestamp;
-        this.username = username;
-    }
-
-    public Message( int senderId,int groupId,int receiverId,String message,Date timestamp){
+    public Message(int messageId,int senderId,String message,String timestamp){
         this.senderId = senderId;
-        this.groupId = groupId;
-        this.receiverId = receiverId;
+    this.messageId = messageId;
         this.message = message;
         this.timestamp = timestamp;
     }
 
-    public Message(int messageId, String message,Date timestamp,String username,int imageId){
-        this.messageId = messageId;
-        this.message = message;
-        this.timestamp = timestamp;
-        this.username = username;
-        this.imageId = imageId;
-    }
-
-    public Message(int messageId, String message, String strTimestamp, String username, Bitmap bitmap) {
-        this.messageId = messageId;
-        this.message = message;
-        this.strTimestamp = strTimestamp;
-        this.username = username;
-        this.bitmap = bitmap;
-    }
 
     @Override
     public int getMessageId() {
@@ -100,12 +75,12 @@ public class Message implements IMessage {
     }
 
     @Override
-    public Date getTimestamp() {
+    public String getTimestamp() {
         return this.timestamp;
     }
 
     @Override
-    public void setTimestamp(Date timestamp) {
+    public void setTimestamp(String timestamp) {
         this.timestamp = timestamp;
     }
 
@@ -145,15 +120,6 @@ public class Message implements IMessage {
         this.strTimestamp = strTimestamp;
     }
     @Override
-    public Bitmap getBitmap() {
-        return this.bitmap;
-    }
-    @Override
-    public void setBitmap(Bitmap bitmap) {
-        this.bitmap = bitmap;
-    }
-
-    @Override
     public boolean isMe() {
         return isMe;
     }
@@ -162,34 +128,4 @@ public class Message implements IMessage {
         isMe = me;
     }
 
-    public static ArrayList<IMessage> getObjectList() {
-
-        ArrayList<IMessage> dataList = new ArrayList<>();
-
-        IMessage chat = new Message(1,"Hi",new Date(),"user01",R.drawable.human);
-        chat.setMe(false);
-        dataList.add(chat);
-
-        IMessage chat2 = new Message(2,"hello",new Date(),"user01",R.drawable.ic_supervisor_account_black_24dp);
-        chat2.setMe(false);
-        dataList.add(chat2);
-
-        IMessage chat3 = new Message(3,"Fine, how about you?",new Date(),"user01",0);
-        chat3.setMe(false);
-        dataList.add(chat3);
-
-        IMessage chat4 = new Message(4,"Really Good!",new Date(),"user01",R.drawable.ic_folder_shared_black_24dp);
-        chat4.setMe(true);
-        dataList.add(chat4);
-
-        IMessage chat5 = new Message(5,"It was really fun playing with you last month.",new Date(),"user01",R.drawable.ic_phone_android_black_24dp);
-        chat5.setMe(false);
-        dataList.add(chat5);
-
-        IMessage chat6 = new Message(6,"Well, the meal in particular was good flavour. If you have time, I'd like to go to dinner with you again.",new Date(),"user01",0);
-        chat6.setMe(true);
-        dataList.add(chat6);
-
-        return dataList;
-    }
 }
