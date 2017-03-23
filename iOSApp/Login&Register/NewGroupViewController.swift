bd0e8ff18899cd593c38769d4435ceebe18b3ea3
Tudor Zugravu <tudorzugravu@Tudors-MacBook-Pro.local>
2017-03-23 16:46:13 +0000

    create group UI + camera functionality

--- iOSApp/Login&Register/NewGroupViewController.swift
+++ iOSApp/Login&Register/NewGroupViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class NewGroupViewController: UIViewController {
+class NewGroupViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var groupNameTextField: UITextField!
     @IBOutlet weak var groupDescriptionTextField: UITextField!
@@ -16,9 +16,11 @@ class NewGroupViewController: UIViewController {
     @IBOutlet weak var selectedImage: UIImageView!
     
     let groupModel=GroupModel()
+    let picker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        picker.delegate = self
 
         SocketIOManager.sharedInstance.setDisconnectedListener(completionHandler: { (userList) -> Void in
             print("disconnected");
@@ -27,11 +29,36 @@ class NewGroupViewController: UIViewController {
         })
         SocketIOManager.sharedInstance.setGlobalPrivateListener(completionHandler: { () -> Void in })
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        selectedImage.layer.shadowColor = UIColor.lightGray.cgColor
+        selectedImage.layer.shadowRadius = 5
+        selectedImage.layer.shadowOpacity = 0.6
+        selectedImage.clipsToBounds = false
+    }
 
     @IBAction func cameraButtonPressed(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.allowsEditing = false
+            picker.sourceType = UIImagePickerControllerSourceType.camera
+            picker.cameraCaptureMode = .photo
+            picker.modalPresentationStyle = .fullScreen
+            present(picker,animated: true,completion: nil)
+        } else {
+            let alertView = UIAlertController(title: "Failed",
+                                              message: "The camera is not available" as String, preferredStyle:.alert)
+            let okAction = UIAlertAction(title: "Done", style: .default, handler: nil)
+            alertView.addAction(okAction)
+            self.present(alertView, animated: true, completion: nil)
+        }
     }
     
-    @IBOutlet weak var photosButtonPressed: UIButton!
+    @IBAction func photosButtonPressed(_ sender: Any) {
+        picker.allowsEditing = false
+        picker.sourceType = .photoLibrary
+        picker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
+        present(picker, animated: true, completion: nil)
+    }
     
     @IBAction func backButtonPressed(_ sender: Any) {
         let _ = navigationController?.popViewController(animated: true)
@@ -45,6 +72,17 @@ class NewGroupViewController: UIViewController {
 //            displayAlertMessage(mymessage: "All fields are required!");
 //        }
 
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        let chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage //2
+        selectedImage.contentMode = .scaleAspectFit //3
+        selectedImage.image = chosenImage //4
+        dismiss(animated:true, completion: nil) //5
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
     }
 
 }
